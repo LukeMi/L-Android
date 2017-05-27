852f41c5a901c280d5e2f3fd83313c0b9fc56955
chenmz <chenmz@91360.com>
2017-05-27 17:42:53 +0800

    []

---
+++ LukeAndroidDemo/app/src/main/java/com/lukemi/myandroid/evenbus/BackEvenBUSFragment.java
@@ -0,0 +1,104 @@
+package com.lukemi.myandroid.evenbus;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.lukemi.myandroid.R;
+import com.lukemi.myandroid.util.CommonUtils;
+import com.lukemi.myandroid.util.HttpUtils;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link BackEvenBUSFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class BackEvenBUSFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private View view;
+    private ImageView img;
+    private TextView text;
+    private Button btn;
+
+
+    public BackEvenBUSFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment BackEvenBUSFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static BackEvenBUSFragment newInstance(String param1, String param2) {
+        BackEvenBUSFragment fragment = new BackEvenBUSFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_back_even_bu, container, false);
+        text = ((TextView) view.findViewById(R.id.text_bd));
+        img = ((ImageView) view.findViewById(R.id.img_bd));
+        btn = ((Button) view.findViewById(R.id.btn_click));
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getContext(), ForgroundActivity.class));
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onDestroy() {
+        EventBus.getDefault().unregister(this);
+        super.onDestroy();
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onMessageEvent(String event) {
+        text.setText("EvenBus");
+        String url = "https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/bd_logo1_31bdc765.png";
+        CommonUtils.glideLoadPic(getContext(), url, img);
+    }
+
+    ;
+}
