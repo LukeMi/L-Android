3f02ff44339aed39d5010daa4e7e5ff00c43629d
chenmingzhi <chenmingzhi@ccclubs.com>
2019-01-08 20:57:15 +0800

    modify

--- 03Code/L_Android/app/src/main/java/com/lukemi/android/tutorial/sessionlifecycle/Session2Activity.java
+++ 03Code/L_Android/app/src/main/java/com/lukemi/android/tutorial/sessionlifecycle/Session2Activity.java
@@ -1,10 +1,13 @@
 package com.lukemi.android.tutorial.sessionlifecycle;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.lukemi.android.tutorial.R;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class Session2Activity extends AppCompatActivity {
 
@@ -12,5 +15,13 @@ public class Session2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_session2);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.task)
+    public void onViewClicked() {
+        Intent intent = new Intent(this,NewTaskActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
