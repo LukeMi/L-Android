fd447ba69e4ce76e5aab4e4a55543383ddb6bf90
chenmingzhi <chenmingzhi@ccclubs.com>
2019-06-18 13:55:23 +0800

    android 自带TTS（文本转语音）功能

---
+++ 03Code/L_Android/app/src/main/java/com/lukemi/android/tutorial/TTSActivity.java
@@ -0,0 +1,69 @@
+package com.lukemi.android.tutorial;
+
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.lukemi.android.common.util.Logcat;
+import com.lukemi.android.tutorial.util.ToastUtil;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * @author mzchen
+ * @date 2019/6/18 11:47
+ * @des 语音合成Demo
+ * @mail chenmingzhiji@163.com
+ */
+public class TTSActivity extends AppCompatActivity {
+    TextToSpeech tts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tts);
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (tts != null && tts.isSpeaking()) {
+            tts.stop();
+        }
+    }
+
+    @OnClick({R.id.btn_init, R.id.btn_play, R.id.btn_stop, R.id.btn_destroy})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn_init:
+                if (tts == null) {
+                    tts = new TextToSpeech(getApplicationContext(), status -> Logcat.log("status : " + status), getLocalClassName());
+                }
+                break;
+            case R.id.btn_play:
+                if (tts != null) {
+                    tts.speak("接到实时单，请及时抢单，1S", TextToSpeech.QUEUE_FLUSH, null);
+                } else {
+                    ToastUtil.show_makeText(getApplication(), "u must have the TextToSpeech instance", Toast.LENGTH_SHORT);
+                }
+                break;
+            case R.id.btn_stop:
+                if (tts != null && tts.isSpeaking()) {
+                    tts.stop();
+                }
+                break;
+            case R.id.btn_destroy:
+                if (tts != null && tts.isSpeaking()) {
+                    tts.stop();
+                }
+                tts = null;
+                break;
+            default:
+                break;
+        }
+    }
+}
