827fe27992a30808693b6607b9e9183e183c97c4
chenmingzhi <chenmingzhi@ccclubs.com>
2019-12-31 01:52:37 +0800

    FragmentManager 的 transaction 动画效果

--- 03Code/L_Android/widget/src/main/java/com/jeferry/android/widget/fragment/FragmentActivity.java
+++ 03Code/L_Android/widget/src/main/java/com/jeferry/android/widget/fragment/FragmentActivity.java
@@ -5,11 +5,12 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.FrameLayout;
 
 import com.jeferry.android.widget.R;
 
 public class FragmentActivity extends AppCompatActivity {
+
+    private int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,25 +20,30 @@ public class FragmentActivity extends AppCompatActivity {
     }
 
     private void init() {
-        for (int i = 0; i < 3; i++) {
-            ParamsFragment fragment = ParamsFragment.newInstance(i + 1);
+        findViewById(R.id.tv_add_fragment).setOnClickListener(this::onCLick);
+        findViewById(R.id.tv_back_fragment).setOnClickListener(this::onCLick);
+    }
+
+    private void onCLick(View view) {
+        if (view.getId() == R.id.tv_add_fragment) {
+            ParamsFragment fragment = ParamsFragment.newInstance(++i);
             addFragment(fragment);
+        } else if (view.getId() == R.id.tv_back_fragment) {
+            if (getSupportFragmentManager().getBackStackEntryCount() > 1) {
+                getSupportFragmentManager().popBackStack();
+            } else {
+                finish();
+            }
         }
-        findViewById(R.id.tv_fragment_back).setOnClickListener(this::onCLick);
     }
 
     private void addFragment(Fragment fragment) {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         // 添加到回退站
-        transaction.addToBackStack("");
-        transaction.add(R.id.fragment, fragment).commitAllowingStateLoss();
-    }
-
-    private void onCLick(View view) {
-        if (getSupportFragmentManager().getBackStackEntryCount() > 1) {
-            getSupportFragmentManager().popBackStack();
-        } else {
-            finish();
-        }
+        transaction.addToBackStack("")
+                .setCustomAnimations(R.anim.anim_right_in, R.anim.anim_left_out,
+                        R.anim.anim_right_in, R.anim.anim_left_out)
+                .add(R.id.fragment, fragment)
+                .commitAllowingStateLoss();
     }
 }
