89900a74888d4689a852664b1240c1719f7be687
chenmingzhi <chenmingzhi@ccclubs.com>
2020-11-02 16:11:26 +0800

    tab 使用

--- 03Code/L_Android/widget/src/main/java/com/jeferry/android/widget/wechat/tablayout/TabLayoutActivity.java
+++ 03Code/L_Android/widget/src/main/java/com/jeferry/android/widget/wechat/tablayout/TabLayoutActivity.java
@@ -1,10 +1,12 @@
 package com.jeferry.android.widget.wechat.tablayout;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 import android.widget.Toast;
-
 
 import com.jeferry.android.widget.R;
 import com.lukemi.android.common.util.ToastUtil;
@@ -19,6 +21,7 @@ import com.lukemi.android.common.util.ToastUtil;
 public class TabLayoutActivity extends AppCompatActivity {
 
     private TabLayout tl;
+    private TabLayout tl2;
 
     private String[] titleList = new String[]{"android", "ios", "python"};
 
@@ -28,11 +31,12 @@ public class TabLayoutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_tab_layout);
 
         initView();
-
+        selectDefault(0);
     }
 
     private void initView() {
         tl = findViewById(R.id.tl);
+        tl2 = findViewById(R.id.tl2);
         for (String s : titleList) {
             TabLayout.Tab tab = tl.newTab();
             tab.setText(s);
@@ -54,5 +58,53 @@ public class TabLayoutActivity extends AppCompatActivity {
 
             }
         });
+        for (int i = 0; i < titleList.length; i++) {
+            TabLayout.Tab tab = tl2.newTab();
+            tab.setCustomView(R.layout.item_tab);
+            ((TextView) tab.getCustomView().findViewById(R.id.tv_tab)).setText(titleList[i]);
+            tl2.addTab(tab);
+        }
+        tl2.addOnTabSelectedListener(new TabLayout.BaseOnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                String s = ((TextView) tab.getCustomView().findViewById(R.id.tv_tab)).getText().toString();
+                ToastUtil.show_makeText(getApplicationContext(), s, Toast.LENGTH_SHORT);
+                select(tab);
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+    private void select(TabLayout.Tab selectTab) {
+        for (int i = 0; i < tl2.getTabCount(); i++) {
+            TabLayout.Tab tab = tl2.getTabAt(i);
+            TextView viewById = tab.getCustomView().findViewById(R.id.tv_tab);
+            boolean select = selectTab == tab;
+            setTabAttrs(viewById, select, R.color.red, R.color.color_black);
+        }
+    }
+
+    private void selectDefault(int index) {
+        TabLayout.Tab tab = tl2.getTabAt(index);
+        TextView viewById = tab.getCustomView().findViewById(R.id.tv_tab);
+        setTabAttrs(viewById, true, R.color.red, R.color.color_black);
+    }
+
+    private void setTabAttrs(TextView viewById, boolean select, int p, int p2) {
+        int size = select ? 16 : 14;
+        int colorId = select ? p : p2;
+        Typeface typeface = select ? Typeface.DEFAULT_BOLD : Typeface.DEFAULT;
+        viewById.setTextSize(size);
+        viewById.setTextColor(ContextCompat.getColor(getApplication(), colorId));
+        viewById.setTypeface(typeface);
     }
 }
