493e3687635a93093ffa2328155c48384ae5947d
chenmingzhi <chenmingzhi@ccclubs.com>
2020-11-09 22:18:24 +0800

    reset

--- 03Code/L_Android/app/src/main/java/com/lukemi/android/tutorial/di/IGlobalSettingObserver.java
+++
@@ -1,9 +0,0 @@
-package com.lukemi.android.tutorial.di;
-
-import android.content.SharedPreferences;
-
-public interface IGlobalSettingObserver {
-    void onSaveData(SharedPreferences.Editor editor);
-
-    void onLoadData(SharedPreferences sp);
-}
--- 03Code/L_Android/compiler/src/main/java/com/jeferry/android/compiler/MyClass.java
+++
@@ -1,4 +0,0 @@
-package com.jeferry.android.compiler;
-
-public class MyClass {
-}
